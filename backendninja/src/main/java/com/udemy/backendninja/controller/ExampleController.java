4df67c6eef485cf01c132021a0d1a4eefa84d634
Vicente Noriega <vicente.noriega@corp.pounceconsulting.com>
2018-07-03 16:46:17 -0500

    SE 4; Spring MVC - Los controladores; 20. Insertando datos simples en plantillas.

--- backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -1,9 +1,9 @@
 package com.udemy.backendninja.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,16 +12,15 @@ public class ExampleController {
 	
 	public static final String EXAMPLE_VIEW = "example";
 	
-//	@RequestMapping(value="/exampleString", method=RequestMethod.GET)
 	@GetMapping("/exampleString")
-	public String exampleString() {
+	public String exampleString(Model model) {
+		model.addAttribute("name", "Donald");
 		return EXAMPLE_VIEW;
 	}
 
-//	@RequestMapping(value="/exampleMAV", method=RequestMethod.GET)
 	@GetMapping("/exampleMAV")
 	public ModelAndView exampleMAV() {
-		return new ModelAndView(EXAMPLE_VIEW);
+		return new ModelAndView(EXAMPLE_VIEW).addObject("name", "Micky");
 	}
 	
 }
