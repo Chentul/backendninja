470c8541639a663d2f7a220586450c27d5e2ad95
Vicente Noriega <vicente.noriega.sc@gmail.com>
2018-07-05 22:01:44 -0500

    SE 4; Spring MVC - Los controladores; 32. Nuestro primer componente.

--- backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -3,12 +3,15 @@ package com.udemy.backendninja.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.backendninja.component.ExampleComponent;
 import com.udemy.backendninja.model.Person;
 
 @Controller
@@ -17,8 +20,14 @@ public class ExampleController {
 	
 	public static final String EXAMPLE_VIEW = "example";
 	
+	// inject the component
+	@Autowired
+	@Qualifier("exampleComponent")
+	private ExampleComponent exampleComponent; // we don't need the keyword new
+	
 	@GetMapping("/exampleString")
 	public String exampleString(Model model) {
+		exampleComponent.sayHello();
 		model.addAttribute("people", this.getPeople());
 		return EXAMPLE_VIEW;
 	}
